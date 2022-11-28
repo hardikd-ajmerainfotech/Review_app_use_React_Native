e1bafe3a011e5ccf744c3443117fa10f408af1d6
unknown <hardik.dadukiya@ajmerainfotech.com>
2022-11-28 19:21:51 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Font from "expo-font";
+import React, { useState } from "react";
+import Home from "./screens/Home";
+import AppLoading from "expo-app-loading";
+import Navigator from './routes/HomeStack'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const getFonts = () =>
+  Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
+
+export  function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  if (fontsLoaded) {
+    return <Navigator />;
+  } else {
+    return (
+      <AppLoading 
+      startAsync={getFonts} 
+      onFinish={() => setFontsLoaded(true)} 
+      onError={()=>console.log('error')}/>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
